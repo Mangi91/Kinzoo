d5a75afcb0049249872a9a9c92233669f82d2f4b
Manuel Cubillo <24781450+Mangi91@users.noreply.github.com>
2021-06-06 19:46:45 -0500

    Initial Commit

---
+++ Kinzoo/KinzooApp.swift
@@ -0,0 +1,17 @@
+//
+//  KinzooApp.swift
+//  Kinzoo
+//
+//  Created by Manuel Cubillo on 6/6/21.
+//
+
+import SwiftUI
+
+@main
+struct KinzooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
