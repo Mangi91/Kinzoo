812de5c74bcebfdaa1a36c0a141155d82c196905
Manuel Cubillo <24781450+Mangi91@users.noreply.github.com>
2021-06-07 19:08:51 -0500

    Implemented HeaderView and added dummy images

--- Kinzoo/ContentView.swift
+++ Kinzoo/ContentView.swift
@@ -9,13 +9,42 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            HeaderView()
+            Spacer()
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct HeaderView: View {
+    var body: some View {
+        HStack {
+            HStack {
+                Image("mike")
+                    .resizable()
+                    .frame(width: 32, height: 32)
+                    .cornerRadius(20)
+                Text("Mike")
+                    .font(.system(size: 16, weight: .semibold, design: .default))
+                Image("down-arrow")
+                    .renderingMode(.template)
+                    .resizable()
+                    .frame(width: 10, height: 10)
+                    .foregroundColor(.gray)
+            }
+            Spacer()
+            Image("settings")
+                .renderingMode(.template)
+                .resizable()
+                .frame(width: 25, height: 25)
+                .foregroundColor(Color(red: 97/255, green: 65/255, blue: 217/255))
+        }
+        .padding(.horizontal, 16)
     }
 }
